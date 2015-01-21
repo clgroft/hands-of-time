63723c821dde19c35850294d8f154ededed5180c
Chad Groft <clgroft@gmail.com>
2015-01-21 11:47:58 -0800

    Add bool array of available spaces to game state

--- handsoftime.hs
+++ handsoftime.hs
@@ -5,26 +5,34 @@ type Position = Int
 type Board = (Int, UArray Position Int)
 type Path = [Position]
 
-makeBoard :: [Int] -> Board
-makeBoard ns = (size, listArray (0,size - 1) ns) where size = length ns
+boardBounds :: Int -> (Position, Position)
+boardBounds size = (0, size - 1)
 
-data GameState = GameState [Position] Int
+makeBoard :: [Int] -> Board
+makeBoard ns = (size, listArray (boardBounds size) ns) where size = length ns
+
+data GameState = GameState [Position] (UArray Position Bool) Int
+
+startState :: Int -> GameState
+startState size =
+  GameState [] (listArray (boardBounds size) (replicate size True)) size
 
 steps :: Board -> GameState -> [Position]
-steps (size, _)     (GameState []     _) = [0..size - 1]
-steps (size, steps) (GameState (p:ps) _) =
+steps (size, _)     (GameState []     _     _) = [0..size - 1]
+steps (size, steps) (GameState (p:ps) avail _) =
   map (`mod` size) [p + step, p + size - step] where step = steps!p
 
 validPathsFrom :: Board -> GameState -> [Path]
-validPathsFrom board state@(GameState ps numAvail)
+validPathsFrom board state@(GameState ps avail numAvail)
   | numAvail == 0	= [reverse ps]
   | otherwise     =
-    [ path | p <- steps board state, not (p `elem` ps),
-             path <- validPathsFrom board $ GameState (p:ps) (numAvail - 1) ]
+    [ path | p <- steps board state, avail!p,
+             path <- validPathsFrom board $
+                GameState (p:ps) (avail // [(p, False)]) (numAvail - 1) ]
 
 main = do
   putStrLn "Input the puzzle as a list of Ints:"
   boardList <- liftM read getLine
   putStrLn "Calculating..."
   let board@(size, _) = makeBoard boardList
-  print . head . validPathsFrom board $ GameState [] size
+  print . head . validPathsFrom board $ startState size
