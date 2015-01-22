36f93e7dcc7f9637a3c13ca91a6b31a20000d97b
Chad Groft <clgroft@gmail.com>
2015-01-21 17:45:26 -0800

    GameState has Set of positions instead of UArray of Bools

--- handsoftime.hs
+++ handsoftime.hs
@@ -1,6 +1,8 @@
+import Prelude hiding (null)
 import Control.Monad (liftM)
 import Data.Array.Unboxed
 import Data.Ix (range)
+import Data.Set hiding (filter)
 
 type Position = Int -- technically, Int modulo size of board
 type Board = (Int, UArray Position Int) -- = (size, step from each position)
@@ -10,33 +12,28 @@ makeBoard :: [Int] -> Board
 makeBoard ns = (size, listArray (0, size - 1) ns) where size = length ns
 
 data GameState = GameState {
-  currPos  :: Maybe Position,       -- there is no position at the beginning
-  avail    :: UArray Position Bool, -- easier checking & updating than a list
-  numAvail :: Int                   -- for efficiency
+  currPos :: Maybe Position, -- there is no position at the beginning
+  avail   :: Set Position    -- easier checking & updating than a list
 }
 
 startState :: Board -> GameState
-startState (size, steps) =
-  GameState Nothing
-            -- all positions are free at the start
-            (listArray (bounds steps) (repeat True))
-            size
+-- all positions are free at the start
+startState (size, steps) = GameState Nothing $ fromList (indices steps)
 
 validSteps :: Board -> GameState -> [Position]
 -- every move is valid at the beginning
-validSteps (_,    steps) (GameState Nothing _ _) = range $ bounds steps
-validSteps (size, steps) (GameState (Just p) avail _) =
-  filter (avail!) $ map (`mod` size) [p + step, p + size - step]
+validSteps (_,    steps) (GameState Nothing _) = indices steps
+validSteps (size, steps) (GameState (Just p) avail) =
+  filter (`member` avail) [(p + step) `mod` size, (p + size - step) `mod` size]
     where step = steps!p
 
 moveTo :: Position -> GameState -> GameState
-moveTo p (GameState _ avail numAvail) =
-  GameState (Just p) (avail // [(p, False)]) (numAvail - 1)
+moveTo p (GameState _ avail) = GameState (Just p) $ delete p avail
 
 validPathsFrom :: Board -> GameState -> [Path]
-validPathsFrom board state@(GameState _ _ numAvail)
-  | numAvail == 0 = [[]]
-  | otherwise     =
+validPathsFrom board state@(GameState _ avail)
+  | null avail = [[]]
+  | otherwise  =
     [ p:path | p <- validSteps board state,
                path <- validPathsFrom board $ moveTo p state ]
 
